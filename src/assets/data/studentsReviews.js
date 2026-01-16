b4e41c081db5d97612f21a3d4d03cc9e7ee2fbdf
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2026-01-16 11:05:17 +0530

    reviews updated

--- src/assets/data/studentsReviews.js
+++ src/assets/data/studentsReviews.js
@@ -2,85 +2,79 @@ const studentsData = [
   {
     image:
       "https://cdn.dribbble.com/userupload/44901540/file/b6559312506b193a1ec994715b4d6b15.png",
-    name: "Anshika Trivedi",
+    name: "Dr. Anshika Trivedi",
     subReview:
       " Vidya Jeewan - More Than Just a Place, An Experience. Some places change you, shape you, and fill you with the motivation you always ",
     review:
-      " Vidya Jeewan - More Than Just a Place, An Experience. Some places change you, shape you, and fill you with the motivation you always longed for. For me, that place was Vidya Jeewan.Choosing to be a part of it was the best decision I ever made.very morning, Apurv Sir’s powerful words set the tone for the day - his motivation wasn’t just about pushing forward; it was about believing in yourself. ",
+      " Vidya Jeewan - More Than Just a Place, An Experience. Some places change you, shape you, and fill you with the motivation you always longed for. For me, that place was Vidya Jeewan. Choosing to be a part of it was the best decision I ever made. Very morning, Apurv Sir’s powerful words set the tone for the day - his motivation wasn’t just about pushing forward, It was about believing in yourself. ",
   },
   {
     image:
       "https://cdn.dribbble.com/userupload/44901545/file/9eca1be1a67a45c76fd8e9353a78bee5.png",
-    name: "Saurabh Dhamdhere",
+    name: "Dr. Saurabh Dhamdhere",
     subReview:
-      " Early morning motivation sessions led by Dr Apurv Sir have been particularly inspiring, helping me alot to start the day with a positive ",
+      " Early morning motivation sessions led by Dr. Apurv Sir have been particularly inspiring, helping me alot to start the day with a positive ",
     review:
-      " Early morning motivation sessions led by Dr Apurv Sir have been particularly inspiring, helping me alot to start the day with a positive mindset and renewed energy. The study materials and resources provided are comprehensive and up - to - date, which is best for NEET PG preparation. ",
+      " Early morning motivation sessions led by Dr. Apurv Sir have been particularly inspiring, helping me alot to start the day with a positive mindset and renewed energy. The study materials and resources provided are comprehensive and up - to - date, which is best for NEET PG preparation. ",
   },
   {
     image:
       "https://cdn.dribbble.com/userupload/44901543/file/9246a80d0ca7efd8b05bf5600caafbf4.png",
-    name: "Shubh Dahiya",
+    name: "Dr. Shubh Dahiya",
     subReview:
-      " BTR Bootcamp by Dr Zainab Ma'am✨❤️ This place has such positive vibes.The best thing about this place is that they not only teach you ",
+      " This place has such positive vibes. The best thing about this place is that they not only teach you academics but life lessons too. ",
     review:
-      " BTR Bootcamp by Dr Zainab Ma'am✨❤️ This place has such positive vibes.The best thing about this place is that they not only teach you academics but life lessons too. Apurv Sir and Ravi Sir makes it the best. I'll never forget this phase of my life where I have learnt so much from my teachers. I love the environment here and enthusiasm of the students. There is very cooperative staff and everyone here helps you too. Topiary is also amazing. Much Respect 🙏✨😇 ",
+      " This place has such positive vibes. The best thing about this place is that they not only teach you academics but life lessons too. Dr. Apurv Sir makes it the best. I'll never forget this phase of my life where I have learnt so much from my teachers. I love the environment here and enthusiasm of the students. There is very cooperative staff and everyone here helps you too, Much Respect 🙏✨😇. ",
   },
   {
     image:
       "https://cdn.dribbble.com/userupload/44901542/file/b8fb7cd63422c13a5087ab629cf7b2ba.png",
-    name: "Deepak Pandey",
+    name: "Dr. Deepak Pandey",
     subReview:
-      " One of the best academy : academic excellence praise the quality of teaching , curriculum and academic support. Faculty and staff : recognize ",
+      " One of the best academy : academic excellence praise the quality of teaching, curriculum and academic support. Faculty and staff : recognize ",
     review:
-      " One of the best academy : academic excellence praise the quality of teaching , curriculum and academic support. Faculty and staff : recognize the dedication , expertise , and aroachability of professors and staff members and thankyou to dr. Apurv sir to making vidya jeevan 🙏🏻 ",
+      " One of the best academy : academic excellence praise the quality of teaching, curriculum and academic support. Faculty and staff : recognize the dedication, expertise and aroachability of professors and staff members and thankyou to Dr. Apurv sir to making vidya jeevan 🙏🏻. ",
   },
   {
     image:
       "https://cdn.dribbble.com/userupload/44901547/file/9e97ffa1c78e3b9721e78e628f54ca2c.png",
-    name: "Rupenika Gaur",
+    name: "Dr. Rupenika Gaur",
     subReview:
-      " This is the best place to study. The teachers, the staff, the students everyone there is so good. I would like to thank Cerebellum Academy, Dr. ",
+      " This is the best place to study. The teachers, the staff, the students everyone there is so good. I would like to thank Dr. Apurv Mehra Sir ",
     review:
-      " This is the best place to study. The teachers, the staff, the students everyone there is so good. I would like to thank Cerebellum Academy, Dr. Gobind Rai Garg Sir (my favourite), Dr. Apurv Mehra Sir for making this beautiful place for us, Zainab Ma’am for everything she do for us to be better, and a big thanks to Dr. Ravi Sharma Sir for mentoring personally to whoever in self doubt. I can’t express how grateful I’m to be their student. Also I would like to thank Ashish Sir who manages everything so nicely, Mama ji for being so positive and always greeting us with smile. This place is literally my “Home away from home“. ",
+      " This is the best place to study. The teachers, the staff, the students everyone there is so good. I would like to thank Dr. Apurv Mehra Sir for making this beautiful place for us. I can’t express how grateful I’m to be their student. This place is literally my “Home away from home“. ",
   },
   {
     image:
       "https://cdn.dribbble.com/userupload/44901546/file/605e2add7b334e2bb4d8ca12990f2563.png",
-    name: "Sahil Sharma",
+    name: "Dr. Sahil Sharma",
     subReview:
-      " Everyday Apurv sir used to come and used to motivate all of us in the early morning. He used to inspire everyone about life, to be a better person ",
+      " Everyday Dr. Apurv sir used to come and used to motivate all of us in the early morning. He used to inspire everyone about life, to be a better person ",
     review:
-      " Everyday Apurv sir used to come and used to motivate all of us in the early morning. He used to inspire everyone about life, to be a better person and a good doctor. Ravi sir used to motivate everyone and clear everyone's query personally about everything. I have to say that I was lucky that I got the opportunity to be enrolled in the first ever Bootcamp. Thank you ",
+      " Everyday Dr. Apurv sir used to come and used to motivate all of us in the early morning. He used to inspire everyone about life, to be a better person and a good doctor. I have to say that I was lucky that I got the opportunity to be enrolled. Thank you ",
   },
   {
     image:
       "https://cdn.dribbble.com/userupload/44901542/file/b8fb7cd63422c13a5087ab629cf7b2ba.png",
-    name: "Prajj",
+    name: "Dr. Prajj",
     subReview:
-      " A big thank you to Zainab Ma’am for tirelessly teaching us and inspiring us to reach greater heights. Supporting us in many ways. To Apurv sir ",
+      " A big thank you to Dr. Apurv sir for constantly motivating us. Addressing our concerns and making us feel seen and heard. They are so understanding. ",
     review:
-      " A big thank you to Zainab Ma’am for tirelessly teaching us and inspiring us to reach greater heights. Supporting us in many ways. To Apurv sir for constantly motivating us. Addressing our concerns and making us feel seen and heard. They are so understanding. They help you unlock your true potential, provided you learn from the wisdom they provide. A word of gratitude to the entire team as well - they relentlessly work hard to provide us with whatever we might need. ",
+      " A big thank you to Dr. Apurv sir for constantly motivating us. Addressing our concerns and making us feel seen and heard. They are so understanding. They help you unlock your true potential, provided you learn from the wisdom they provide. A word of gratitude to the entire team as well - they relentlessly work hard to provide us with whatever we might need. ",
   },
   {
     image:
       "https://cdn.dribbble.com/userupload/44901544/file/30c9fd2ec226d6c52d092de03b572632.png",
-    name: "Kanishka Singh",
+    name: "Dr. Kanishka Singh",
     subReview:
       " Vidya jeevan is a gorgeous place. There are some places, some experiences that teach us a lot. This is one such place. Thankful that we had ",
     review:
-      " Vidya jeevan is a gorgeous place. There are some places, some experiences that teach us a lot. This is one such place. Thankful that we had great mentors, Dr Zainab Ma'am, Dr Apoorv Sir and Dr Ravi Sir🙏 They have not only motivated us to be good doctors but to be good humans as well. I have never enjoyed learning as much as it was with Dr Zainab ma'am. She makes learning fun. Very wholesome and very gorgeous experience✨ ",
+      " Vidya jeevan is a gorgeous place. There are some places, some experiences that teach us a lot. This is one such place. Thankful that we had great mentor Dr. Apurv Sir They have not only motivated us to be good doctors but to be good humans as well. Very wholesome and very gorgeous experience✨ ",
   },
-  // {
-  //   image: Female_Avatar,
-  //   name: "Clinical Cases Simplified",
-  //   review:
-  //     " Simply the best not only in terms of knowledge but also the values that have been engraved within us by Dr. Apurv sir, Dr. Zainab mam and Dr. Ravi sir. I am so grateful to exist in this era so that I can experience the greatness with my own eyes. As the name says, we all got a new 'jeevan' by learning this 'vidya' of how and what to study. Also, Team Cerebellum has always been so cooperative and supportive that you won't have to worry about anything else except studying. ",
-  // },
   {
     image:
       "https://cdn.dribbble.com/userupload/44901541/file/337d14b3ab0a559f20d265f545ccd9f9.png",
-    name: "Mansi Ahuja 73",
+    name: "Dr. Mansi Ahuja",
     subReview:
       " Thank u so much to whole Vidya Jeevan team And special thanks to Dr Zainab Ma’am Dr Ravi sir and Dr Apurv sir Who made best educational",
     review:
